48c918ee22b10caf71b1cfb7e96b4803594fafc4
Keyrock2 <mbrock1047@gmail.com>
2025-09-30 13:44:18 +0900

    과제제출

--- student.cpp
+++ student.cpp
@@ -1,34 +1,35 @@
 #include "student.h"
-
 #include <iostream>
 
 /* StudentRecord */
 
-StudentRecord::StudentRecord(float midterm, float final) 
-/* TODO: Initializer list 사용해 초기화 해보기 */
-{}
-
-float StudentRecord::getTotal() const {
-    /* TODO */
-    return 0.0f;
+StudentRecord::StudentRecord(float midterm, float final) : midterm(midterm), final(final)
+{
 }
 
-void StudentRecord::updateScores(float midterm, float final) {
-    /* TODO: this keyword 사용해 보기 */
+float StudentRecord::getTotal() const
+{
+    return midterm + final;
 }
 
+void StudentRecord::updateScores(float midterm, float final)
+{
+    this->midterm = midterm;
+    this->final = final;
+}
 
 /* Student */
-
-Student::Student() 
-/* TODO: Initializer list 사용해 초기화 해보기 */
-{ }
-
-Student::Student(const char *name, int id, float midterm, float final)
-/* TODO: Initializer list 사용해 초기화 해보기 */
-{  }
-
-void Student::updateRecord(const char *name, float midterm, float final) {
-    /* TODO */
+Student::Student() : name(""), id(0), record(0.0f, 0.0f)
+{
 }
 
+Student::Student(const char *name, int id, float midterm, float final)
+    : name(name), id(id), record(midterm, final)
+{
+}
+
+void Student::updateRecord(const char *name, float midterm, float final)
+{
+    this->name = name;
+    this->record.updateScores(midterm, final);
+}
--- student_manager.cpp
+++ student_manager.cpp
@@ -4,52 +4,133 @@
 /* StudentManager */
 int StudentManager::last_student_id = 1000;
 
-void StudentManager::addStudent(char const* name, float midterm, float final) {
-    int id = ++last_student_id;
-    /* TODO */
-
+void StudentManager::addStudent(const char *name, float midterm, float final)
+{
+    if (num_of_students < MAX_STUDENTS)
+    {
+        int id = ++last_student_id;
+        students[num_of_students] = Student(name, id, midterm, final);
+        num_of_students++;
+    }
 }
 
-void StudentManager::deleteStudent(int id) {
-    /* TODO */
+void StudentManager::deleteStudent(int id)
+{
+    int idx = findStudentByID(id);
+    if (idx != -1)
+    {
+        for (int i = idx; i < num_of_students - 1; ++i)
+        {
+            students[i] = students[i + 1];
+        }
+        num_of_students--;
+    }
 }
 
-void StudentManager::modifyStudent(const Student& student) {
-    /* TODO */
+void StudentManager::modifyStudent(const Student &student)
+{
+    int idx = findStudentByID(student.getID());
+    if (idx != -1)
+    {
+        students[idx] = student;
+    }
 }
 
-int StudentManager::findStudentByID(int id) const {
-    /* TODO */
+int StudentManager::findStudentByID(int id) const
+{
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        if (students[i].getID() == id)
+        {
+            return i;
+        }
+    }
     return -1;
 }
 
-int StudentManager::findBestStudentInMidterm() const {
-    /* TODO */
-    return -1;
+int StudentManager::findBestStudentInMidterm() const
+{
+    if (num_of_students == 0)
+        return -1;
+    float maxScore = -1.0f;
+    int bestId = -1;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        if (students[i].getRecord().getMidterm() > maxScore)
+        {
+            maxScore = students[i].getRecord().getMidterm();
+            bestId = students[i].getID();
+        }
+    }
+    return bestId;
 }
 
-int StudentManager::findBestStudentInFinal() const {
-    /* TODO */
-    return -1;
+int StudentManager::findBestStudentInFinal() const
+{
+    if (num_of_students == 0)
+        return -1;
+    float maxScore = -1.0f;
+    int bestId = -1;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        if (students[i].getRecord().getFinal() > maxScore)
+        {
+            maxScore = students[i].getRecord().getFinal();
+            bestId = students[i].getID();
+        }
+    }
+    return bestId;
 }
 
-int StudentManager::findBestStudent() const {
-    /* TODO */
-    return -1;
+int StudentManager::findBestStudent() const
+{
+    if (num_of_students == 0)
+        return -1;
+    float maxTotal = -1.0f;
+    int bestId = -1;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        if (students[i].getRecord().getTotal() > maxTotal)
+        {
+            maxTotal = students[i].getRecord().getTotal();
+            bestId = students[i].getID();
+        }
+    }
+    return bestId;
 }
 
-float StudentManager::getMidtermAverage() const {
-    /* TODO */
-    return 0.0f;
+float StudentManager::getMidtermAverage() const
+{
+    if (num_of_students == 0)
+        return 0.0f;
+    float total = 0.0f;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        total += students[i].getRecord().getMidterm();
+    }
+    return total / num_of_students;
 }
 
-float StudentManager::getFinalAverage() const {
-    /* TODO */
-    return 0.0f;
+float StudentManager::getFinalAverage() const
+{
+    if (num_of_students == 0)
+        return 0.0f;
+    float total = 0.0f;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        total += students[i].getRecord().getFinal();
+    }
+    return total / num_of_students;
 }
 
-float StudentManager::getTotalAverage() const {
-    /* TODO */
-    return 0.0f;
+float StudentManager::getTotalAverage() const
+{
+    if (num_of_students == 0)
+        return 0.0f;
+    float total = 0.0f;
+    for (int i = 0; i < num_of_students; ++i)
+    {
+        total += students[i].getRecord().getTotal();
+    }
+    return total / num_of_students;
 }
-
